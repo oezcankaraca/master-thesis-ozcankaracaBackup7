83de20cc1055f5ee3fcc001bbe6b2e52fe3f5596
Özcan Karaca <oezcan.karaca@stud.tu-darmstadt.de>
2024-02-10 08:49:39 +0000

    last changes for testbed

--- run-testbed.sh
+++ run-testbed.sh
@@ -4,8 +4,8 @@ printf "\nStarting Testbed.\n\n"
 
 # run-testbed-automation.sh
 export has_superpeer_values="false true"
-export number_of_peers_values="5 10"
-export choice_of_pdf_mb_values="128 64"
+export number_of_peers_values="5 10 20 35 50 75"
+export choice_of_pdf_mb_values="2 4 8 16 32"
 
 # Base directory path where all project-related files are located
 export BASE_PATH="$HOME/Desktop"
